2080e91a3eb7096dcef9d5f3829fd3affb19220a
MahaDaiki <mahadaiki2@gmail.com>
2024-12-12 09:26:18 +0100

    GDCD-2 configuration

---
+++ src/main/java/org/example/assuranceapp/service/serviceImplementation/CustomUserDetailServiceImpl.java
@@ -0,0 +1,14 @@
+package org.example.assuranceapp.service.serviceImplementation;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+@NoArgsConstructor
+public class CustomUserDetailServiceImpl {
+
+    public CustomUserDetailServiceImpl(String s) {
+    }
+}
